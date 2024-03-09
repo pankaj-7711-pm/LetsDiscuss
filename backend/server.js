79b560f9293a2e2721fa90882e74b7f1029ac41c
pankaj-7711-pm <pankajmandalplt58@gmail.com>
2024-03-09 17:37:22 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,39 @@ const userRoutes = require("./routes/userRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
+const path = require("path");
 dotenv.config();
 connectDB();
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("hii");
-});
+
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running Successfully");
+  });
+}
+
+// --------------------------deployment------------------------------
+
 
 app.use(notFound);
 app.use(errorHandler);
